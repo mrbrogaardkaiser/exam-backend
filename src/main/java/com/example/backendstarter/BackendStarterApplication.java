6beddaf9dad1a54533607373b2916a851a6af85d
Lars <larsbrogaard86@gmail.com>
2022-05-23 12:20:02 +0200

    init

---
+++ src/main/java/com/example/backendstarter/BackendStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendStarterApplication.class, args);
+    }
+
+}
