cebbc7750c10a5e27e25cb62c9ac11bde7d67c8b
Lars <larsbrogaard86@gmail.com>
2022-05-29 12:10:17 +0200

    Error handling

--- src/main/java/com/example/backendstarter/configuration/TestData.java
+++ src/main/java/com/example/backendstarter/configuration/TestData.java
@@ -24,7 +24,7 @@ public class TestData implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        //makeData();
+        makeData();
 
     }
 }
